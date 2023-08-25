9c7b687ea680a640e6ad93137aea4a9fff5e4f4d
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2023-08-25 08:34:58 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+# Calcula o acréscimo a partir dos juros e os juros a partir do acréscimo
+# Versões
+# 0.1 24/08/2023 Primeiros métodos e propriedades, para testes
+# 0.2 25/08/2023 Cálculos e testes com prints
+import Juros
+
+juros = Juros.Juros(3, True, 30)
+juros.setpagamentos(",", "0,30,60")
+juros.setpesos(",", "2,1,1")
+
+print(juros.Quantidade)
+print(juros.Composto)
+print(juros.Periodo)
+print(juros.Pagamentos[0])
+print(juros.Pagamentos[1])
+print(juros.Pagamentos[2])
+print(juros.Pesos[0])
+print(juros.Pesos[1])
+print(juros.Pesos[2])
+print(juros.getpesototal())
+print(juros.jurosparaacrescimo(2))
+print(juros.acrescimoparajuros(1.4826375341396858, 18))
